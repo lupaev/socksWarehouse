7206eac5085af4d9964c1ec5a75543850960b6cd
sergej <lupaevsv@gmail.com>
2023-03-29 13:46:48 +0700

    init

---
+++ src/main/java/com/skypro/sockswarehous/SocksWarehousApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.sockswarehous;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocksWarehousApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocksWarehousApplication.class, args);
+    }
+
+}
