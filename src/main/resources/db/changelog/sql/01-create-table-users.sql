c7eb3c0ca4859d3c4a530d6d9ce3ed5ba4c7adad
sergej <lupaevsv@gmail.com>
2023-06-05 01:22:15 +0700

    fix sql

--- src/main/resources/db/changelog/sql/01-create-table-users.sql
+++ src/main/resources/db/changelog/sql/01-create-table-users.sql
@@ -1,3 +1,6 @@
+--liquibase formatted sql
+
+--changeset sergej:1
 create table socks
 (
     id BIGSERIAL PRIMARY KEY,
--- src/main/resources/db/changelog/sql/02-data-users.sql
+++ src/main/resources/db/changelog/sql/02-data-users.sql
@@ -1,1 +1,4 @@
+--liquibase formatted sql
+
+--changeset sergej:2
 insert into socks (color, cotton_part, quantity) values ('red', 20, 1111);
